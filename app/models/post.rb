15601db99357cd40309c4595a65452509168a5a6
saramccombs <theSaraMcCombs@gmail.com>
2020-01-26 16:33:57 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,19 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     self.post_categories.build(category: category)
+  #   end
+  # end
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 end
